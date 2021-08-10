36d01eac94ba6cb11927c1b3a3139195157595c9
Kittl, Chris <chris.kittl@tu-dortmund.de>
2021-08-10 23:40:15 +0200

    Updating result structure for pandapower results

--- tcv/calculation/pandapower/TwoWindingTestBench.py
+++ tcv/calculation/pandapower/TwoWindingTestBench.py
@@ -1,14 +1,15 @@
 import datetime as datetime
 from math import cos, pi, sin, sqrt, atan
 
+import numpy as np
 import pandapower
 import pandapower as pp
 from numpy.ma import arange
 
-from tcv.calculation.result import GridResultTwoWinding
+from tcv.calculation.pandapower.TestGrid import TapSide, TransformerModel, test_grid_two_winding
+from tcv.calculation.result.GridResultTwoWinding import GridResultTwoWinding
 from tcv.calculation.pandapower import ResultWriter
 from tcv.calculation.pandapower.TestBench import TestBench, __calc_current_angle
-from tcv.calculation.pandapower import test_grid_two_winding, TapSide, TransformerModel
 
 
 def extract_results(net: pandapower.pandapowerNet = None):
@@ -23,8 +24,7 @@ def extract_results(net: pandapower.pandapowerNet = None):
     """
     # Nodal voltages at the low voltage node of the transformer
     v_pu = net.res_bus.vm_pu[1]
-    e_pu = v_pu * cos(net.res_bus.va_degree[1] / 180 * pi)  # Real part of nodal voltage
-    f_pu = v_pu * sin(net.res_bus.va_degree[1] / 180 * pi)  # Imaginary part of nodal voltage
+    v_ang_degree = net.res_bus.va_degree[1]
 
     # Power at high voltage node
     p_hv_kw = net.res_trafo.p_hv_mw[0] * 1000.0
@@ -42,13 +42,12 @@ def extract_results(net: pandapower.pandapowerNet = None):
 
     # Current at high voltage node
     i_mag_lv_a = net.res_trafo.i_lv_ka[0] * 1000.0  # Port current at high voltage node
-    phi_v_lv_degree = atan(f_pu / e_pu) / pi * 180.0
-    i_ang_lv_degree = __calc_current_angle(p_lv_kw, q_lv_kvar, phi_v_lv_degree)
+    i_ang_lv_degree = __calc_current_angle(p_lv_kw, q_lv_kvar, v_ang_degree)
 
-    return GridResultTwoWinding(e_pu=e_pu, f_pu=f_pu, v_pu=v_pu, p_hv_kw=p_hv_kw, q_hv_kvar=q_hv_kvar, s_hv_kva=s_hv_kva,
-                                i_mag_hv_a=i_mag_hv_a,
-                                i_ang_hv_degree=i_ang_hv_degree, p_lv_kw=p_lv_kw, q_lv_kvar=q_lv_kvar, s_lv_kva=s_lv_kva,
-                                i_mag_lv_a=i_mag_lv_a, i_ang_lv_degree=i_ang_lv_degree)
+    return GridResultTwoWinding(v_lv_pu=v_pu, v_ang_lv_degree=v_ang_degree, p_hv_kw=p_hv_kw, q_hv_kvar=q_hv_kvar,
+                                s_hv_kva=s_hv_kva, i_mag_hv_a=i_mag_hv_a, i_ang_hv_degree=i_ang_hv_degree,
+                                p_lv_kw=p_lv_kw, q_lv_kvar=q_lv_kvar, s_lv_kva=s_lv_kva, i_mag_lv_a=i_mag_lv_a,
+                                i_ang_lv_degree=i_ang_lv_degree)
 
 
 class TwoWindingTestBench(TestBench):
@@ -56,9 +55,8 @@ class TwoWindingTestBench(TestBench):
     def __init__(self):
         super().__init__()
 
-    def calculate(self, tap_min=-10, tap_max=10, p_min=-1.0, p_max=1.0, p_nom_mw=0.4, p_step_size: float = 0.1,
-                  v_ref_kv=0.4, s_ref_mva=0.4,
-                  tap_side=TapSide.LV, transformer_model=TransformerModel.PI):
+    def calculate(self, tap_min=-10, tap_max=10, p_min=-1.0, p_max=1.0, s_nom_mva: float = 0.63, p_step: int = 21,
+                  v_ref_kv=0.4, s_ref_mva=0.4, tap_side=TapSide.LV, transformer_model=TransformerModel.PI):
         """
         Performs a series of power flow calculations with pandapower and the transformer test bench. It iterates through
         all permissible tap positions and further sweeps the range of permissible power infeed or consumption.
@@ -71,8 +69,8 @@ class TwoWindingTestBench(TestBench):
             tap_max (int): Maximum permissible tap position
             p_min (float): Minimum permissible active power (negative = infeed) in p.u.
             p_max (float): Maximum permissible active power (negative = infeed) in p.u.
-            p_nom_mw (float): Nominal power of the load in MW
-            p_step_size (float): Step size to use for sweeping over the range of active power
+            s_nom_mva (float): Nominal power of the load in MW
+            p_step (float): Amount of ticks along active power axis
             v_ref_kv (float): Nominal voltage of the reference system in kV
             s_ref_mva (float): Nominal apparent power of the reference system in MVA
             tap_side (TapSide): Position of the tap changer
@@ -82,58 +80,30 @@ class TwoWindingTestBench(TestBench):
         self.logger.info(
             "Starting to calculate grid with pandapower. Parameters: tap = %i...%i, p = (%.2f...%.2f)*%.2f MW, "
             "reference = %.2f MVA @ %.2f kV, tap side = %s" %
-            (tap_min, tap_max, p_min, p_max, p_nom_mw, s_ref_mva, v_ref_kv, tap_side))
+            (tap_min, tap_max, p_min, p_max, s_nom_mva, s_ref_mva, v_ref_kv, tap_side))
         self.logger.info("Preparing the general information")
         tap_range = range(tap_min, tap_max + 1)  # Range of available tap positions
-        p_range = arange(p_min, p_max + p_step_size,
-                         p_step_size)  # Active power range in terms of dimensionless power from infeed to load
+        p_range = [round(p_pu * s_nom_mva * 1000) / 1000 for p_pu in
+                   np.linspace(-1.0, 1.0, p_step)]  # Power range @ mv port
 
         # --- Prepare the output dictionary ---
-        out = {}
+        out = []
 
         # --- Iterate through all available tap positions ---
         tap_pos: int
         for tap_pos in tap_range:
-            out[str(tap_pos)] = []
             for p in p_range:
                 # Perform the calculation
                 self.logger.debug(
                     "Power flow with tap position = %i and p = %.3f p.u. (%3.f MW)" % (
-                        tap_pos, p, p * p_nom_mw))
-                net = test_grid_two_winding(tap_pos, p * p_nom_mw, s_ref_mva, tap_side)
+                        tap_pos, p, p * s_nom_mva))
+                net = test_grid_two_winding(tap_pos, p * s_nom_mva, s_ref_mva, tap_side)
                 pp.runpp(net, trafo_model=transformer_model.value)
 
                 # Extract the result of this model run
                 result = extract_results(net)
 
                 # Register the results
-                out[str(tap_pos)].append({'p': p, 'result': result})
+                out.append({'tap_pos': tap_pos, 'p_lv': p, 'result': result})
 
         return out
-
-    def __write_results(self, base_directory: str = "", file_name_suffix: str = "",
-                        time_stamp_pattern: str = "%Y%m%d-%H%M", results: dict = None):
-        """
-        Iterates over a dictionary of results and writes the content to a file. The file name pattern is
-        '<date_time_string of now>_<file_name_suffix>.csv'.
-
-        Parameters:
-            base_directory (str): The base directory, where the file shall be placed
-            file_name_suffix (str): Suffix of the file name, giving it a meaning
-            time_stamp_pattern (str): Pattern, on how to convert the current date time to a string
-            results (dict): Dictionary mapping the tap position to a list of tuples (active power and result class
-                            object)
-        """
-        # Set up the writer
-        date_time_string = datetime.datetime.now().strftime(time_stamp_pattern)
-        file_name = date_time_string + '_' + file_name_suffix + '.csv'
-        self.logger.debug("Writing results to file '%s' in directory '%s'." % (file_name, base_directory))
-
-        writer = ResultWriter(base_directory, date_time_string)
-        for key in results.keys():
-            tap_pos = int(key)
-            for value in results[key]:
-                p_pu = value[0]  # The first entry is the active power in p.u.
-                result = value[1]
-                writer.write_result(tap_pos, p_pu, result)
-        writer.shutdown()
